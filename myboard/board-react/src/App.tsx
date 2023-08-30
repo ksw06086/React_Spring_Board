ec02915061bd5676b50089e0d177091a873a28cc
unknown <k-sunwo@naver.com>
2023-08-30 21:20:54 +0900

    Connection : React, Spring Connect

--- myboard/board-react/src/App.tsx
+++ myboard/board-react/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [connection, setConnection] = useState<string>('');
+
+  const connectionTest = () => {
+    axios.get('http://localhost:4000/').then((res) => {
+      setConnection(res.data);
+    }).catch((err) => {
+      setConnection(err.message);
+    });
+  }
+
+  useEffect(()=>{
+    connectionTest();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{connection}</p>
       </header>
     </div>
   );
