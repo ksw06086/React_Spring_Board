75bb2bac0e01df8503e9d57c7ed9d0fdd058ecbd
unknown <k-sunwo@naver.com>
2023-08-31 14:59:52 +0900

    AUTH : SignUp 기능 추가

--- myboard/board-react/src/App.tsx
+++ myboard/board-react/src/App.tsx
@@ -1,29 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import Athentication from './views/Athentication';
 
 function App() {
-  const [connection, setConnection] = useState<string>('');
-
-  const connectionTest = () => {
-    axios.get('http://localhost:4000/').then((res) => {
-      setConnection(res.data);
-    }).catch((err) => {
-      setConnection(err.message);
-    });
-  }
-
-  useEffect(()=>{
-    connectionTest();
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{connection}</p>
-      </header>
+      <Athentication />
     </div>
   );
 }
---
+++ myboard/myboard/src/main/java/com/swkim/myboard/dto/SignUpDto.java
@@ -0,0 +1,23 @@
+package com.swkim.myboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// <DTO>
+// - view에서 데이터를 Controller로 가져올 때 따로따로가 아니라 
+//   객체 하나로 한번에 가져올 수 있음 
+// - DTO 객체로 만들어 view에서 보내주는 데이터를 한번에 객체에 넣어 가져와 사용함
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpDto {
+	private String userEmail;
+	private String userPassword;
+	private String userPasswordCheck;
+	private String userNickname;
+	private String userPhoneNumber;
+	private String userAddress;
+	private String userAddressDetail;
+}
