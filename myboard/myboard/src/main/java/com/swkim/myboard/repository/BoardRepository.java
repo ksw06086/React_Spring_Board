602e7724bb802d1701f45638f0ca814d4726fb65
unknown <k-sunwo@naver.com>
2023-09-01 16:25:46 +0900

    BOARD : 주간 Top3 게시물, 최신 게시물 리스트, Top10 인기 검색어 API 기능 구현

--- myboard/myboard/src/main/java/com/swkim/myboard/repository/BoardRepository.java
+++ myboard/myboard/src/main/java/com/swkim/myboard/repository/BoardRepository.java
@@ -1,5 +1,8 @@
 package com.swkim.myboard.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.swkim.myboard.entity.BoardEntity;
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 	
+	public List<BoardEntity> findTop3ByBoardWriteDateAfterOrderByBoardLikeCountDesc(Date boardWriteDate);
+	
+	public List<BoardEntity> findByOrderByBoardWriteDateDesc();
 }
---
+++ myboard/myboard/src/main/java/com/swkim/myboard/service/BoardService.java
@@ -0,0 +1,65 @@
+package com.swkim.myboard.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.swkim.myboard.dto.ResponseDto;
+import com.swkim.myboard.entity.BoardEntity;
+import com.swkim.myboard.entity.PopularSearchEntity;
+import com.swkim.myboard.repository.BoardRepository;
+import com.swkim.myboard.repository.PopularSearchRepository;
+
+@Service
+public class BoardService {
+	
+	@Autowired BoardRepository boardRepository;
+	@Autowired PopularSearchRepository popularSearchRepository;
+	
+	public ResponseDto<List<BoardEntity>> getTop3() {
+		List<BoardEntity> boardList = new ArrayList<BoardEntity>();
+		Date date = Date.from(Instant.now().minus(7, ChronoUnit.DAYS));
+		
+		try {
+			boardList = boardRepository.findTop3ByBoardWriteDateAfterOrderByBoardLikeCountDesc(date);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseDto.setFailed("DataBase Error!");
+		}
+		
+		return ResponseDto.setSuccess("Success!", boardList);
+	}
+	
+	public ResponseDto<List<BoardEntity>> getList() {
+		
+		List<BoardEntity> boardList = new ArrayList<BoardEntity>();
+		
+		try {
+			boardList = boardRepository.findByOrderByBoardWriteDateDesc();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseDto.setFailed("Database Error!");
+		}
+		
+		return ResponseDto.setSuccess("Success!", boardList);
+	}
+	
+	public ResponseDto<List<PopularSearchEntity>> getPopularSearchList(){
+		List<PopularSearchEntity> popularSearchList = new ArrayList<PopularSearchEntity>();
+		
+		try {
+			popularSearchList = popularSearchRepository.findTop10ByOrderByPopularSearchCountDesc();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseDto.setFailed("Database Error!");
+		}
+		
+		return ResponseDto.setSuccess("Success!", popularSearchList);
+	}
+	
+}
