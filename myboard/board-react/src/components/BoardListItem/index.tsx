2dbf3aac91f06e5047fb1ffabcd75ff87adfb362
swkim <53504073+ksw06086@users.noreply.github.com>
2023-09-25 18:02:39 +0900

    views 화면 구성한거 제거

---
+++ myboard/board-react/src/components/BoardListItem/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BoardListItem() {
+  return (
+    <div>index</div>
+  )
+}
--- myboard/board-react/src/views/BoardMain/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BoardMain() {
-  return (
-    <div style={{ height: '100vh', backgroundColor: '#ffd3dd'}}>BoardMain</div>
-  )
-}
