8bcddcdb18d42d4398988e2a3623695751a2debf
swkim <53504073+ksw06086@users.noreply.github.com>
2023-11-06 17:56:13 +0900

    회원가입 화면 구현 중

--- myboard/myboard/src/main/java/com/swkim/myboard/dto/response/ResponseDto.java
+++ myboard/myboard/src/main/java/com/swkim/myboard/dto/response/ResponseDto.java
@@ -22,4 +22,9 @@ public class ResponseDto {
 		ResponseDto responsebody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responsebody);
 	}
+
+	public static ResponseEntity<ResponseDto> validationFailed() {
+		ResponseDto responsebody = new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responsebody);
+	}
 }
