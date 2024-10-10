690c0493f433641fabdaea08640421698f48983c
swkim <53504073+ksw06086@users.noreply.github.com>
2024-10-10 13:04:54 +0900

    유저 정보 조회 API 구현

--- myboard/myboard/src/main/java/com/swkim/myboard/service/UserService.java
+++ myboard/myboard/src/main/java/com/swkim/myboard/service/UserService.java
@@ -1,10 +1,12 @@
 package com.swkim.myboard.service;
 
 import com.swkim.myboard.dto.response.user.GetSignInUserResponseDto;
+import com.swkim.myboard.dto.response.user.GetUserResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
 
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
 
 }
--- myboard/myboard/src/main/java/com/swkim/myboard/service/implement/UserServiceImpl.java
+++ myboard/myboard/src/main/java/com/swkim/myboard/service/implement/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.swkim.myboard.service.implement;
 
 import com.swkim.myboard.dto.response.ResponseDto;
 import com.swkim.myboard.dto.response.user.GetSignInUserResponseDto;
+import com.swkim.myboard.dto.response.user.GetUserResponseDto;
 import com.swkim.myboard.entity.UserEntity;
 import com.swkim.myboard.repository.UserRepository;
 import com.swkim.myboard.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,24 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public ResponseEntity<? super GetUserResponseDto> getUser(String email) {
+
+        UserEntity userEntity = null;
+
+        try {
+
+            userEntity = userRepository.findByEmail(email);
+            if (userEntity == null) return GetUserResponseDto.noExistUser();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetUserResponseDto.success(userEntity);
+    }
 
     @Override
     public ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email) {
